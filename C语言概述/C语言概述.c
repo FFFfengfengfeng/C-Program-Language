bb5f3bc138297b42fadd043a61d15109faceac2a
FFFfengfengfeng <1098865109@qq.com>
2018-09-03 14:07:47 +0800

    no message

--- C语言概述/C语言概述.c
+++ C语言概述/C语言概述.c
@@ -1,4 +1,16 @@
 /**
  * 2.1 简单的C程序示例
- *      
+ *     
  */ 
+
+#include<stdio.h>
+int main(void)
+{
+	int num;
+	num = 1;
+	
+	printf("简单的C语言实例\n");
+	printf("我最喜欢的数字是: %d.\n", num);
+	
+	return 0; 
+} 
