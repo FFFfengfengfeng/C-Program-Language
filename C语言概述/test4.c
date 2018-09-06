8343c5da3b05e1450331ec91f5f15d3ea034fe1c
FFFfengfengfeng <1098865109@qq.com>
2018-09-06 13:20:48 +0800

    no message

--- C语言概述/test4.c
+++ C语言概述/test4.c
@@ -4,5 +4,25 @@
  * For he's a jolly good fellow! 
  * For he's a jolly good fellow! 
  * Which nobody can deny!
- 
+ * 除了main()函数以外,该程序还要调用两个自定义函数,一个名为jolly(),一个名为deny() 
  */
+ 
+#include<stdio.h>
+void jolly(void);
+void deny(void);
+
+int main(void) {
+	jolly();
+	jolly();
+	jolly();
+	
+	deny();
+}
+
+void jolly(void) {
+	printf("For he's a jolly good fellow!\n");
+}
+void deny(void) {
+	printf("Which nobody can deny!\n");
+} 
+
